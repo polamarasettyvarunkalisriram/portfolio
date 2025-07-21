511c6bc2daa024911a6b94d1413c7df7d510b57c
Polamarasetty Varun Kali Sriram <75966558+polamarasettyvarunkalisriram@users.noreply.github.com>
2025-07-21 17:26:57 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -18,7 +18,7 @@ I love building clean and user-friendly web applications.
 Always learning and excited to work on meaningful projects.
           </p>
           <div className="flex flex-wrap gap-4">
-  <a href="https://drive.google.com/file/d/1nyw44eMHi2CAPUdIUETQ-rYUvVp6NIH7/view?usp=drivesdk" target="_blank" rel="noopener noreferrer">
+  <a href="https://drive.google.com/file/d/1RrOr1nHWGGnQJkYfpklX5AQTBRv6ltWT/view?usp=drive_link" target="_blank" rel="noopener noreferrer">
     <Button size="lg" className="rounded-full">
       Resume
     </Button>
